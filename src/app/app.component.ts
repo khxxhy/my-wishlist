39ce9b4358d1041ef612f4ae6646c429a0a71872
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-08-04 12:48:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { WishItem } from 'src/shared/models/wishItems';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items : WishItem[]= [
+    new WishItem('be happy'),
+    new WishItem('communicate'),
+    new WishItem('love'),
+    new WishItem('learn angular', true)
+  ];
   title = 'my-wishlist';
 }
