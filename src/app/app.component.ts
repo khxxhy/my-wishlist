54087f7d39658264f05aa071b90cf7e159008595
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-08-08 10:52:53 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,31 @@ export class AppComponent {
     new WishItem('love', true),
     new WishItem('learn angular')
   ];
+
+  listFilter: string = "0";
+
+  newWishText = " ";
+
   title = 'my-wishlist';
+
+  visibleItems : WishItem[] = this.items;
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText="";
+  }
+
+  filterChanged(value : any) {
+   if(value === '0') {
+    this.visibleItems = this.items;
+   }
+   else if (value === '1') {
+    this.visibleItems = this.items.filter(item => !item.isComplete);
+   }
+   else if (value === '2') {
+    this.visibleItems = this.items.filter(item => item.isComplete);
+   }
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
