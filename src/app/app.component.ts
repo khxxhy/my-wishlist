cb6f56ee1556d0b7064b1c2df6ee34db177ca81f
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-08-13 15:00:31 +0530

    adding services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { WishService } from './wish.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  items!: WishItem[];
+  items! : WishItem[];
 
-  constructor(events: EventService, private WishService: WishService) {
+  constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish: any) => {
       let index = this.items.indexOf(wish);
       this.items.splice(index, 1);
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    this.WishService.getWishes().subscribe((data: any) => {
+    this.wishService.getWishes().subscribe((data: any) => {
       this.items = data;
     })
   }
