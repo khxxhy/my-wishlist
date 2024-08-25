1d5e6858133cfa7adb462aac6d7bf25e30dc98b7
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-08-25 22:45:02 +0530

    form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,14 +13,14 @@ export class ContactComponent implements OnInit {
   }
 
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl('')
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [Validators.required, Validators.minLength(10)])
   });
 
 
 submitForm(){
-  console.log("this.contactForm.value")
+  console.log(this.contactForm.valid)
   //if(
    // this.senderNameControl.dirty){
     //  alert("you changedthe name field.")
